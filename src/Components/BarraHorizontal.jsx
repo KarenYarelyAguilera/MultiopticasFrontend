ea869896a86cb2f1c7973681fb4ecdc6857e633d
SLara26 <seydi.lara@unah.hn>
2023-08-07 11:47:04 -0600

    bitacora Mi perfil y recordatorio

--- src/Components/BarraHorizontal.jsx
+++ src/Components/BarraHorizontal.jsx
@@ -44,7 +44,7 @@ export const BarraHorizontal = (props, { onChange = () => null }) => {
   ];
 
   const urlBitacoraPerfil = 'http://localhost:3000/api/bitacora/perfil';
-
+//comentario para karen
 
 
 
