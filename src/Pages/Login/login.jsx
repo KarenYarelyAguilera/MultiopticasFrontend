af09b6b50235eeec01d9fea5923bc168dc733c49
SLara26 <seydi.lara@unah.hn>
2023-10-22 01:15:50 -0600

    Correciones en perfil, LXV

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -98,7 +98,7 @@ export const Login = props => {
         props.idUsuario(respJsonUsr.data[0].Id_Usuario);
         props.user(respJsonUsr.data[0].Nombre_Usuario);
         props.access(respJsonUsr.data[0].Estado_Usuario)
-        navegate('/loginPrimeraVez'); //configuracion de preguntas//
+        navegate('/preguntasLoginxPV'); //configuracion de preguntas//
       }
       if (
         respJsonPss.data.result &&
