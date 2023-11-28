f2e62eb9450cb375aa100e9ba3f62d32650d529a
SLara26 <seydi.lara@unah.hn>
2023-11-28 14:06:54 -0600

    cambios

--- src/Pages/RecordatorioCitas.jsx
+++ src/Pages/RecordatorioCitas.jsx
@@ -19,6 +19,10 @@ import { useNavigate } from 'react-router';
 
 import axios from 'axios';
 import Select from 'react-select';
+import ReactModal from 'react-modal';
+import { DataGrid, esES } from '@mui/x-data-grid';
+
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';//icono para el boton de bucasr cliente
 
 import { Bitacora } from '../Components/bitacora.jsx';
 
@@ -93,7 +97,64 @@ export const RecordatorioCitas = (props) => {
 
 
 
+  const [pageSize, setPageSize] = useState(5); // Puedes establecer un valor predeterminado
+  const urlCliente = 'http://localhost:3000/api/Expediente';
+  // Nuevo estado para almacenar los clientes
+  const [clientes, setClientes] = useState([]);
+  const [Cliente, setCliente] = useState([]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+
+
+  useEffect(() => {
+    // Cargar la lista de clientes al inicio
+    fetch(urlCliente)
+      .then((response) => response.json())
+      .then((data) => setClientes(data));
+  }, []);
+
+  // Nueva función para seleccionar un cliente
+  const handleSelectCliente = (selectedCliente) => {
+    if (selectedCliente) {
+      setSelectedOption({
+        value: selectedCliente.IdExpediente,
+        label: `${selectedCliente.Cliente} - ${selectedCliente.Nombre}`,
+      });
+      closeModal();
+    }
+  };
+
+  // MAGIA DE SELECCIONAR MALDITASEA
+  const handleCellClick = (params) => {
+    const rowData = params.row;
+    setCliente(rowData)
+    console.log(Cliente.Cliente);
+    closeModal()
+  };
+  const customStyles = {
+    content: {
+      width: '35%', // Ancho de la modal
+      height: '50%', // Alto de la modal
+      margin: 'auto', // Centrar la modal horizontalmente
+      border: '1px solid #ccc',
+      background: '#fff',
+      overflow: 'auto',
+      borderRadius: '4px',
+      outline: 'none',
+    },
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo oscurecido de la modal
+    },
+  };
 
 
 
@@ -131,13 +192,14 @@ export const RecordatorioCitas = (props) => {
 
     // let idCliente = document.getElementById('idClientes').value;
 
-    let idCliente = selectedOption.value; // Usamos el valor de la opción seleccionada
+    let idCliente = Cliente.Cliente; // Usamos el valor de la opción seleccionada
     let Nota = document.getElementById('nota').value;
     // let fecha = document.getElementById('fecha').value
 
     let dataIdCliente = {
       IdCliente: idCliente,
     }
+    console.log(dataIdCliente);
 
 
     await axios.post(urlFechaCita, dataIdCliente).then(response => {
@@ -210,22 +272,22 @@ export const RecordatorioCitas = (props) => {
               <hr />
               <div className="contPrincipalNewCita">
                 <div className="contNewCita">
-                  <TextCustom text="Cliente" className="titleInput" />
-                  <div className="contInput">
-                    {/* <select id="idClientes" className="inputCustomPreguntas">
-                    {tableData.length ? (
-                      tableData.map(pre => (
-                        <option key={pre.idCliente} value={pre.idCliente}>
-                             {`${pre.idCliente} - ${pre.nombre}`}
+                  {/* <TextCustom text="Cliente" className="titleInput" /> */}
+
+                  {/* <select id="idClientes" className="inputCustomPreguntas">
+                      {tableData.length ? (
+                        tableData.map(pre => (
+                          <option key={pre.idCliente} value={pre.idCliente}>
+                              {`${pre.idCliente} - ${pre.nombre}`}
+                          </option>
+                        ))
+                      ) : (
+                        <option value="No existe informacion">
+                          No existe informacion
                         </option>
-                      ))
-                    ) : (
-                      <option value="No existe informacion">
-                        No existe informacion
-                      </option>
-                    )}
-                  </select> */}
-                    <Select
+                      )}
+                    </select> */}
+                  {/* <Select
                       id="idClientes"
                       // className="inputCustomPreguntas"
                       options={tableData.map(pre => ({ value: pre.idCliente, label: `${pre.idCliente} - ${pre.nombre}  ${pre.apellido}` }))}
@@ -233,23 +295,114 @@ export const RecordatorioCitas = (props) => {
                       onChange={setSelectedOption}
                       placeholder="Seleccione un cliente"
                     />
+                    */}
+
+                  {/* <button onClick={openModal}>Seleccionar Cliente</button> */}
 
 
-                  </div>
+
+
+
+
                 </div>
 
 
+
+
+                <div className="contNewCita"  >
+                  <TextCustom text="Cliente" className="titleInput" />
+                  <div className='inputContainer' style={{ display: 'flex', alignItems: 'center' }}>
+
+                    <input
+                      type="text"
+                      //onClick={openModal}
+                      className="inputCustomText"
+                      id="cliente"
+                      placeholder="Seleccione un cliente"
+                      disabled
+                      onChange={handleCellClick}
+                      value={Cliente.Nombre}
+                      style={{ width: '350px' }}
+                    />
+                    <Button className="btnClearFilter" onClick={openModal}><PersonSearchIcon style={{ fontSize: '3rem', marginLeft: '10px' }}></PersonSearchIcon></Button>
+                  </div>
+
+
+
+                </div>
+
+
+                {/* <div>
+                  <label htmlFor="" onChange={handleCellClick}> CLIENTE: {Cliente.Nombre}</label>
+                  <div className="contInput">
+                    <TextCustom text="Cliente:" className="titleInput" />
+                    <div className="contInput">
+                      <button onClick={openModal}>Seleccionar Cliente</button>
+                    </div>
+                  </div>
+                </div> */}
+
+                <ReactModal
+                  style={customStyles}
+                  isOpen={isModalOpen}
+                  onRequestClose={closeModal}
+                  contentLabel="Lista de Clientes"
+                  ariaHideApp={false}>
+                  <div>
+                    <h2>Seleccione un Cliente</h2>
+                    {/* Tabla o cualquier otro componente para mostrar la lista de clientes */}
+                    <DataGrid
+                      rows={clientes}
+                      getRowId={clientes => clientes.IdExpediente}
+                      pagination
+                      autoHeight
+                      onCellClick={handleCellClick}
+                      localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+                      pageSize={pageSize}
+                      rowsPerPageOptions={[5, 10, 50]}
+                      onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                      columns={[
+                        { field: 'Cliente', headerName: 'Identidad', width: 200, headerAlign: 'center' },
+                        { field: 'Nombre', headerName: 'Nombre', width: 300, headerAlign: 'center' }
+                      ]}
+                      style={{ fontSize: '14px' }} // Ajusta el tamaño de la letra aquí
+                      onSelectionModelChange={(selection) => {
+                        // Ensure that selection.selectionModel is defined and not empty
+                        if (selection.selectionModel && selection.selectionModel.length > 0) {
+                          const selectedClientId = selection.selectionModel[0];
+                          const selectedClient = clientes.find(
+                            (client) => client.IdExpediente === selectedClientId
+                          );
+                          // Check if selectedClient is not undefined before calling handleSelectCliente
+                          if (selectedClient) {
+                            handleSelectCliente(selectedClient);
+                          }
+                        }
+                      }}
+
+                    />
+
+                    {/* Botón para cerrar el modal */}
+                    <Button className="btnCloseModal" onClick={closeModal} style={{ fontSize: '16px', fontWeight: 'bold' }}>
+                      Cerrar
+                    </Button>
+                  </div>
+                </ReactModal>
+
+
+
                 {/* 
-              <div className="contNewCita">
-                <TextCustom text="Fecha" className="titleInput" />   */}
+                  <div className="contNewCita">
+                    <TextCustom text="Fecha" className="titleInput" />   */}
                 {/* <DatePicker
-                  type="text"
-                  name=""
-                  maxLength={40}
-                  className="inputCustom"
-                  placeholderText="Fecha"
-                  id="fecha"
-                /> */}
+                      type="text"
+                      name=""
+                      maxLength={40}
+                      className="inputCustom"
+                      placeholderText="Fecha"
+                      id="fecha"
+                  /> 
+                  */}
 
                 {/*     <input type="date" className="inputCustom" id="fecha" ></input>  */}
 
@@ -291,12 +444,12 @@ export const RecordatorioCitas = (props) => {
 
                     onChange={e => setNotas(e.target.value)} //Tambien ponerlo para llamar los datos a la hora de actualizar
                     error={errorNotas}
-                    type="text"
+                    type="textarea"
                     helperText={Msj}
                     name=""
                     maxLength={40}
                     className="inputCustomText"
-                    placeholder="Nota"
+                    placeholder="Ingrese una nota"
                     id="nota"
                     value={Notas}
                   />
@@ -321,8 +474,8 @@ export const RecordatorioCitas = (props) => {
 
                       if (nota === "") {
                         swal("No deje campos vacíos.", "", "error");
-                      } else if (!/^[A-Z]+(?: [A-Z]+)*$/.test(nota)) {
-                        swal("El campo nombre solo acepta letras mayúsculas y solo un espacio entre palabras.", "", "error");
+                      } else if (!/^[A-ZÑ]+(?: [A-ZÑ]+)*$/.test(nota)) {
+                        swal("El campo solo acepta letras mayúsculas y solo un espacio entre palabras.", "", "error");
                       } else if (/(.)\1{2,}/.test(nota)) {
                         setErrorNotas(true);
                         swal("El campo nombre no acepta letras mayúsculas consecutivas repetidas.", "", "error");
@@ -333,6 +486,9 @@ export const RecordatorioCitas = (props) => {
                   </Button>
 
                 </div>
+
+
+
               </div>
             </div>
           </div>
